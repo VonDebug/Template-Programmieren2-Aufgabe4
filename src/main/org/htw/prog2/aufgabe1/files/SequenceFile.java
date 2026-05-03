e805702bac590a17abfa5d9568364f993d28b5d2
adrian <adrian.maroviqi@gmx.net>
2026-05-03 13:11:19 +0200

    Einmal die Struktur meiner Aufgabe 3 in Aufgabe 4 implementiert zur Weiterarbeitung an meinem EIGENEN Code

--- src/main/org/htw/prog2/aufgabe1/files/SequenceFile.java
+++ src/main/org/htw/prog2/aufgabe1/files/SequenceFile.java
@@ -1,31 +1,34 @@
 package org.htw.prog2.aufgabe1.files;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class SequenceFile implements HIVFile {
-    HashSet<String> seqs = new HashSet<>();
-    String firstSeq = "";
 
-    public void addSequence(String sequence) {
-        if(seqs.isEmpty()) {
-            firstSeq = sequence;
-        }
-        seqs.add(sequence);
+    LinkedHashSet<String> orderedSequences = new LinkedHashSet<>();
+
+    public SequenceFile(){
+
     }
 
-    public int getNumberOfSequences() {
-        return seqs.size();
+    public void addSequence(String sequence){
+        orderedSequences.add(sequence);
     }
 
-    public HashSet<String> getSequences() {
-        return seqs;
+    public HashSet<String> getSequences(){
+
+
+        return new HashSet<>(orderedSequences);
     }
 
-    public String getFirstSequence() {
-        return firstSeq;
+    public String getFirstSequence(){
+
+        return orderedSequences.iterator().next();
     }
 
-    public boolean containsSequence(String sequence) {
-        return false;
+    public int getNumberOfSequences(){
+
+        return orderedSequences.size();
     }
+
 }
