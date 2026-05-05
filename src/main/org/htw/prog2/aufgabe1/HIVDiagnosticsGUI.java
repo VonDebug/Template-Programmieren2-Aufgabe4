7ee711d49117acc897106b45c3868232beb62a0a
adrian <asd@>
2026-05-05 16:30:11 +0200

    Grundlegende Gui Elemente außer dem "Best Drug Prediction" sind vorhanden

--- src/main/org/htw/prog2/aufgabe1/HIVDiagnosticsGUI.java
+++ src/main/org/htw/prog2/aufgabe1/HIVDiagnosticsGUI.java
@@ -10,27 +10,65 @@ public class HIVDiagnosticsGUI extends JFrame {
         setSize(400,200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        ImageIcon icon = new ImageIcon("resources/folder_explore.png");
+
         GridBagLayout layout = new GridBagLayout();
-        GridBagConstraints constraints = new GridBagConstraints();
-
         setLayout(layout);
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.weightx = 1;
 
-        add((new JLabel("Please load a mutation CSV file")), constraints);
-        constraints.gridx = 1;
-        constraints.weightx = 3;
-        constraints.anchor = GridBagConstraints.LINE_END;
+        GridBagConstraints c = new GridBagConstraints();
 
-        add(new Button(), constraints);
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem aboutItem = new JMenuItem("About");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        fileMenu.add(aboutItem);
+        fileMenu.add(exitItem);
+
+        jMenuBar.add(fileMenu);
+
+        setJMenuBar(jMenuBar);
 
 
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.insets = new Insets(0, 30, 0, 0);
+        add(new JLabel("Please load a mutation CSV file"), c);
 
+        c.gridx = 1;
+        c.weightx = 3;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.insets = new Insets(0, 0, 0, 30);
+        add(new JButton(icon), c);
 
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.insets = new Insets(0, 30, 0, 0);
+        add(new JLabel("Please load a reference FASTA/FASTQ file"), c);
 
+        c.gridx = 1;
+        c.weightx = 3;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.insets = new Insets(0, 0, 0, 30);
+        add(new JButton(icon), c);
 
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.insets = new Insets(0, 30, 0, 0);
+        add(new JLabel("Please load a patient FASTA/FASTQ file"), c);
 
+        c.gridx = 1;
+        c.weightx = 3;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.insets = new Insets(0, 0, 0, 30);
+        add(new JButton(icon), c);
     }
 
 }
